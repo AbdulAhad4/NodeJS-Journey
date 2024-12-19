d82e9ca3c51a9201cf239dcc2deddd5b85bbfd74
Abdul Ahad <abdul.ahad117266@gmail.com>
2024-12-19 20:03:20 +0600

    Add files via upload

---
+++ Import & Export/export.js
@@ -0,0 +1,21 @@
+// named export
+export const a = 1;
+export const b = 2;
+export const c = 3;
+export const d = 4;
+export const e = 5;
+
+// default export
+const obj = {
+    x:1,
+    y:4
+}
+
+// const over = {
+//     1: 5,
+//     2: 12,           //only one default export in one file
+//     3: 18
+// }
+// export default over;
+
+export default obj;
---
+++ Import & Export/server.js
@@ -0,0 +1,7 @@
+// named import
+import {a, b, c} from "./export.js"
+console.log(a, b, c);
+
+// default import
+import whatever from "./export.js"
+console.log(whatever);
